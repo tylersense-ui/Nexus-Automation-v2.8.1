1e9e24769d37484144ac8cefffd4172aabe4a45a
tylersense-ui <tyler.sense@gmail.com>
2026-02-24 14:34:26 +0100

    Delete Nexus-Automation v2.8.1 directory

--- Nexus-Automation v2.8.1/hack/workers/grow.js
+++
@@ -1,5 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-    if (ns.args[1]) await ns.asleep(ns.args[1]);
-    await ns.grow(ns.args[0]);
-}
--- Nexus-Automation v2.8.1/hack/workers/hack.js
+++
@@ -1,6 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-    // ns.args[0] = cible, ns.args[1] = sleep initial (pour le batching futur)
-    if (ns.args[1]) await ns.asleep(ns.args[1]);
-    await ns.hack(ns.args[0]);
-}
